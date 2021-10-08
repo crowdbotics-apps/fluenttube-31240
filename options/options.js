ce66d0a6adc9563a7ec7a1f2ceedc3cc2e93b77c
Crowdbotics <team@crowdbotics.com>
2021-10-08 14:36:12 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "fluenttube_31240",
+  url: "https://fluenttube_31240.botics.co",
+  api: "https://fluenttube_31240.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
